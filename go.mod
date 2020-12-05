cafe633b105bdb11e609853991321b623e7e9d7e
nnego <sh158371@gmail.com>
2020-12-05 23:38:58 +0800

    add go.sum

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nnego/go_hello
 
 go 1.15
+
+require github.com/gogf/gf v1.14.5
--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gogf/gf"
+)
 
 func main(){
-	fmt.Println("Hello World!")
+	fmt.Println("Hello GF!", gf.VERSION)
 }
